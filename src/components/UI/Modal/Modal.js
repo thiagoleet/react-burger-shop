4bae8a1d53ac62a16bd6ff1f2531c62239ef0ae9
Thiago Leite <thiagofmleite@gmail.com>
2020-11-03 15:16:48 -0300

    Improved performance by using lifecycle checks

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "./Modal.css";
 import Aux from "../../../hoc/aux-hoc";
 import Backdrop from "../Backdrop/Backdrop";
 
-const Modal = (props) => {
-  return (
-    <Aux>
-      <Backdrop clicked={props.modalClosed} show={props.show} />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        }}
-      >
-        {props.children}
-      </div>
-    </Aux>
-  );
-};
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop clicked={this.props.modalClosed} show={this.props.show} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
 
 export default Modal;
