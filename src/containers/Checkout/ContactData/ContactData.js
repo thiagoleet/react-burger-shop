afb1a4759447dbf013f72b4056ef4ff628a47b30
Thiago Leite <thiagofmleite@gmail.com>
2020-11-09 11:21:45 -0300

    Created Input component base

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react'
 import Button from '../../../components/UI/Button/Button'
 import Spinner from '../../../components/UI/Spinner/Spinner'
+import Input from '../../../components/UI/Input/Input'
 import classes from './ContactData.css'
 import service from '../../../services'
+import { elementType } from 'prop-types'
+
+class FormElement {
+  constructor(
+    name = '',
+    value = '',
+    type = 'text',
+    elementType = 'input',
+    placeholder = '',
+    options = null
+  ) {
+    this.elementType = elementType
+    this.value = value
+    this.elementConfig = {
+      type,
+      placeholder,
+      name,
+      options,
+    }
+  }
+}
 
 class ContactData extends Component {
   constructor(props) {
@@ -15,6 +37,29 @@ class ContactData extends Component {
         postalCode: '',
       },
       loading: false,
+      orderForm: {
+        deliveryMethod: new FormElement(
+          'deliveryMethod',
+          '',
+          '',
+          'select',
+          'Delivery Method',
+          [
+            { value: 'fastest', displayValue: 'Fastest' },
+            { value: 'cheapest', displayValue: 'Cheapest' },
+          ]
+        ),
+        email: new FormElement('email', '', 'email', 'input', 'Your E-mail'),
+        name: new FormElement('name', '', 'text', 'input', 'Your Name'),
+        street: new FormElement('street', '', 'text', 'input', 'Street'),
+        zipCode: new FormElement(
+          'postal',
+          '',
+          'number',
+          'input',
+          'Postal Code'
+        ),
+      },
     }
   }
 
@@ -24,16 +69,6 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.totalPrice,
-      customer: {
-        name: 'Max',
-        address: {
-          street: 'Test Street 1',
-          zipCode: '41351',
-          country: 'Germany',
-        },
-        email: 'teste@test.com',
-      },
-      deliveryMethod: 'fastest',
     }
 
     service
@@ -51,32 +86,25 @@ class ContactData extends Component {
   }
 
   render() {
+    const formElements = []
+    for (let key in this.state.orderForm) {
+      formElements.push({
+        id: key,
+        config: this.state.orderForm[key],
+      })
+    }
+
     let form = (
       <form>
-        <input
-          className={classes.Input}
-          type="text"
-          name="name"
-          placeholder="Your name"
-        />
-        <input
-          className={classes.Input}
-          type="email"
-          name="email"
-          placeholder="Your Email"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="street"
-          placeholder="Street"
-        />
-        <input
-          className={classes.Input}
-          type="text"
-          name="postal"
-          placeholder="Postal Code"
-        />
+        {formElements.map((formElement) => (
+          <Input
+            key={formElement.id}
+            elementType={formElement.config.elementType}
+            elementConfig={formElement.config.elementConfig}
+            value={formElement.config.value}
+          />
+        ))}
+
         <Button btnType="Success" clicked={this.orderHandler}>
           ORDER
         </Button>
